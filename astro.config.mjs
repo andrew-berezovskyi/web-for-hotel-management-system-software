5ba5654764878eeaa0f25963f62cb17f25060061
andrew-berezovskyi <aa20062019aa@gmail.com>
2025-11-25 12:24:58 +0200

    Fix download button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
+﻿import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-// import netlify from "@astrojs/netlify";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  integrations: [preact(), tailwind(), compress()],
-  // adapter: netlify()
+    site: "https://thekyivhotel-serm.onrender.com", // <-- вставляєш свою реальну адресу
+    integrations: [preact(), tailwind(), compress()],
 });
